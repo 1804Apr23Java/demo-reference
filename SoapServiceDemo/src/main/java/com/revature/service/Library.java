d52021277dbf37d8c0d23786f3a70ba7eb1a5779
Emily Higgins <emilyrchiggins@gmail.com>
2018-06-05 13:49:29 -0400

    Soap service demo

---
+++ SoapServiceDemo/src/main/java/com/revature/service/Library.java
@@ -0,0 +1,21 @@
+package com.revature.service;
+
+import java.util.List;
+
+import javax.jws.WebService;
+
+import com.revature.model.Book;
+
+@WebService
+public interface Library {
+	
+	/*
+	 * a contract (WSDL) will be generated based on this interface
+	 * go to /Library?wsdl to see the file 
+	 */
+	
+	public List<Book> getAllBooks();
+	
+	public String addBook(Book book);
+
+}
---
+++ SoapServiceDemo/src/main/java/com/revature/service/LibraryImpl.java
@@ -0,0 +1,27 @@
+package com.revature.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jws.WebService;
+
+import com.revature.model.Book;
+
+@WebService(endpointInterface="com.revature.service.Library")
+public class LibraryImpl implements Library {
+
+	@Override
+	public List<Book> getAllBooks() {
+		List<Book> bookList = new ArrayList<>();
+		bookList.add(new Book("Gravity's Rainbow","Thomas Pynchon",1973));
+		bookList.add(new Book("Farenheit 451","Ray Bradbury",1953));
+		bookList.add(new Book("Brave New World","Aldous Huxley",1932));
+		return bookList;
+	}
+
+	@Override
+	public String addBook(Book book) {
+		return "successfully added "+book.getTitle();
+	}
+
+}
